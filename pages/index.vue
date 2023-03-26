e8833e63565778a9a81484899ae5d5a98d2c0c6c
brysonbw <brysonward77@gmail.com>
2023-03-26 15:31:28 -0400

    edit bullet point style

--- pages/index.vue
+++ pages/index.vue
@@ -37,7 +37,9 @@
       <p class="mt-4 text-zinc-600 font-medium dark:text-primary">
         Certificates
       </p>
-      <ul class="list-disc marker:dark:text-primary marker:text-zinc-600 ml-4">
+      <ul
+        class="list-disc marker:dark:text-primary marker:text-zinc-600 ml-[1rem]"
+      >
         <li>
           <a href="/pdf/sec-cert.pdf" target="_blank">Security+ (SY0-601)</a>
         </li>
