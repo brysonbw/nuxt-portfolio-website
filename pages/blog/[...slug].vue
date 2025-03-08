2a73eefc792dc60165898d28effc814e1863d82f
brysonbw <brysonward77@gmail.com>
2025-03-08 15:44:47 -0500

    migrate to nuxt content v3

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -1,26 +1,33 @@
 <template>
-  <article class="rounded-2xl">
-    <ContentDoc
-      v-slot="{ doc }"
-      :path="`/posts/${slug}`"
-    >
-      <!-- Header  -->
-      <header>
-        <div class="text-center p-5">
-          <h1 class="text-3xl md:text-4xl font-bold lg:w-2/3 mx-auto break-words">
-            {{ doc.title }}
-          </h1>
-          <p class="text-gray-500 text-sm mt-2">
-            {{ formatDate(doc.date) }}
-          </p>
+  <section>
+    <div v-if="page">
+      <article class="rounded-2xl">
+        <!-- Header  -->
+        <header>
+          <div class="text-center p-5">
+            <h1 class="text-3xl md:text-4xl font-bold lg:w-2/3 mx-auto break-words">
+              {{ page?.title }}
+            </h1>
+            <p class="text-gray-500 text-sm mt-2">
+              {{ formatDate(page?.meta.date as string) }}
+            </p>
+          </div>
+        </header>
+        <!-- Doc Content -->
+        <div class="mt-4 content p-5">
+          <ContentRenderer
+            :value="page"
+          />
         </div>
-      </header>
-      <!-- Doc Content -->
-      <div class="mt-4 content p-5">
-        <ContentRenderer :value="doc" />
-      </div>
-    </ContentDoc>
-
+      </article>
+    </div>
+    <!-- Empty Placeholder -->
+    <div
+      v-else
+      class="text-center"
+    >
+      Blog post not found. Please try again later.
+    </div>
     <!-- Back To Blog Posts -->
     <div style="margin: 3rem 0 0">
       <NuxtLink
@@ -28,13 +35,17 @@
         to="/blog"
       >← Back to blog posts</NuxtLink>
     </div>
-  </article>
+  </section>
 </template>
 
 <script lang="ts" setup>
 import { formatDate } from '@/utils/formatDate';
 
-const { slug } = useRoute().params;
+const route = useRoute();
+
+const { data: page } = await useAsyncData(route.path, () => {
+  return queryCollection('content').path(route.path).first();
+});
 // TODO: useContentHead for doc page SEO
 </script>
 
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -11,31 +11,18 @@
       <div
         class="w-full sm:w-3/4 lg:w-full mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8 md:gap-10 pt-2"
       >
-        <!-- Blog List  -->
-        <ContentList
-          v-slot="{ list }"
-          path="/posts"
-          fields="title,date"
-          :query="{
-            draft: false,
-            sort: [
-              {
-                date: -1,
-              },
-            ],
-          }"
-        >
-          <!-- Blog Card  -->
+        <slot>
+          <!-- Blog List  -->
           <div
-            v-for="blog in list"
-            :key="blog._path"
+            v-for="blog in blogs"
+            :key="blog.path"
             class="max-w-sm transition ease-in-out hover:scale-105 duration-300 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-[#171717] dark:border-[#2b2b2bc5]"
           >
             <!-- Blog Thumbnail -->
             <div class="h-[300px] relative">
               <img
-                v-if="blog.thumbnail"
-                :src="blog.thumbnail"
+                v-if="blog.meta.thumbnail"
+                :src="blog.meta.thumbnail as string"
                 :alt="blog.title"
                 class="absolute rounded-t-lg w-full h-full object-cover"
               >
@@ -43,20 +30,20 @@
             <!-- Blog Title/Slug Content -->
             <div class="my-4 text-center">
               <h3 class="text-1xl font-bold break-words">
-                <NuxtLink :to="`/blog/${blog.slug}`">
+                <NuxtLink :to="`/blog/${blog.meta.slug}`">
                   {{ blog.title }}
                 </NuxtLink>
               </h3>
               <div class="text-sm text-gray-500 mt-px block">
-                {{ formatDate(blog.date) }}
+                {{ formatDate(blog.meta.date as string) }}
               </div>
               <!-- Blog Tags -->
               <div
-                v-if="blog.tags"
+                v-if="blog.meta.tags"
                 class="mt-2 text-xs"
               >
                 <span
-                  v-for="(tag, index) in blog.tags"
+                  v-for="(tag, index) in blog.meta.tags"
                   :key="tag + index"
                   class="p-1 rounded bg-gray-100 mr-2"
                 >
@@ -64,7 +51,7 @@
               </div>
             </div>
           </div>
-        </ContentList>
+        </slot>
       </div>
     </section>
     <!-- Back To Home Link -->
@@ -83,6 +70,16 @@ import { useAppStore } from '@/stores/app';
 
 // Stores
 const appStore = useAppStore();
+
+const { data: blogs } = await useAsyncData('blog', async () => {
+  const content = await queryCollection('content').all();
+
+  return content.filter(blog => !blog.meta.draft).sort((a, b) => {
+    const aDate = a.meta?.date ? new Date(a.meta.date as string).getTime() : 0;
+    const bDate = b.meta?.date ? new Date(b.meta.date as string).getTime() : 0;
+    return bDate - aDate;
+  });
+});
 
 const { title, meta } = appStore.getSeoMetadata('Bryson Ward | Blog', 'Bryson Ward: Blog Posts', 'blog');
 
